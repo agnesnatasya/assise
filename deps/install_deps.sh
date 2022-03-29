29dbe74a09aeb368bde418e64f845ced39b16444
Agnes Natasya <agnesnatasyawijaya@gmail.com>
2022-03-29 11:05:05 -0600

    feat: leveldb dynamic library dependency

--- deps/install_deps.sh
+++ deps/install_deps.sh
@@ -8,7 +8,7 @@ if [  -n "$(uname -a | grep Ubuntu)" ]; then
 	sudo apt-get install -y build-essential make pkg-config autoconf cmake gcc libcapstone-dev
 	sudo apt-get install -y libudev-dev libnl-3-dev libnl-route-3-dev ninja-build pkg-config valgrind
 	sudo apt-get install -y libnuma-dev libaio1 libaio-dev uuid-dev librdmacm-dev numactl ndctl
-	sudo apt-get install -y libncurses-dev libssl-dev libelf-dev rsync libndctl-dev libdaxctl-dev
+	sudo apt-get install -y libncurses-dev libssl-dev libelf-dev rsync libndctl-dev libdaxctl-dev libleveldb-dev
 
 else
 	echo "Non-Ubuntu OS detected. You may need to install required packages manually (refer to dependencies in README).\n"
